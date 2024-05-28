e6b9235202adebd18e45679e131882bb22c36191
Nguyen Thuy Ngoc Giau <Giauntn0205@gmail.com>
2024-05-28 12:16:36 +0700

    DaoCode

--- src/main/java/com/VooTreeVeeVuu/servicesImp/PartnerServImp.java
+++ src/main/java/com/VooTreeVeeVuu/servicesImp/PartnerServImp.java
@@ -1,8 +1,20 @@
 package com.VooTreeVeeVuu.servicesImp;
 
+import com.VooTreeVeeVuu.entity.Customer;
+import com.VooTreeVeeVuu.entity.Partner;
+import com.VooTreeVeeVuu.repository.PartnerRepository;
 import com.VooTreeVeeVuu.services.PartnerServ;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PartnerServImp implements PartnerServ {
+    @Autowired
+    PartnerRepository partnerRepository;
+
+
 }
+
