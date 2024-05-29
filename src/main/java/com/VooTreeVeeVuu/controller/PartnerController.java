242e8a726b4e4796b2296be07530bc14512fb927
thuanng24 <alex.tran@watatek.com>
2024-05-29 20:32:17 +0700

    update API for Hotel & Partner

--- src/main/java/com/VooTreeVeeVuu/controller/PartnerController.java
+++ src/main/java/com/VooTreeVeeVuu/controller/PartnerController.java
@@ -52,12 +52,15 @@ public class PartnerController {
             pn.setPhoneNum(partners.getPhoneNum());
             pn.setGender(partners.getGender());
             pn.setAge(partners.getAge());
+            pn.setStatus(Partner.Partner_status.ACTIVE);
             Partner updatedPartner = partnerRepository.save(pn);
             return ResponseEntity.ok(updatedPartner);
         } else {
             return ResponseEntity.notFound().build();
         }
     }
+
+
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletePartner(@PathVariable String id) {
